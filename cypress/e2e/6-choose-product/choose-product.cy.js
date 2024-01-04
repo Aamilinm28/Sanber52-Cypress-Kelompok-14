90c91b8994e5a556f2ebae8bc71dab39ed8a691c
mursyidslm <mursyidsalim1@gmail.com>
2024-01-04 14:31:03 +0700

    Fitur Choose Product

---
+++ cypress/e2e/6-choose-product/choose-product.cy.js
@@ -0,0 +1,31 @@
+// import TestloginPage from '../../support/pageObject/testlogin'
+
+// const login = require('../../fixtures/login.json')
+
+
+describe("Login and Choose Product", () => {
+    let signinBtn = '.panel > .header > .authorization-link > a'
+   
+    beforeEach(() => {
+        cy.visit('')
+    })
+
+    it('Choose Product with Login',() => {
+        cy.get(signinBtn).click()
+        cy.get('#email').type('mursyid@gmail.com')
+        cy.get('.login-container > .block-customer-login > .block-content > #login-form > .fieldset > .password > .control > #pass').type('123qweASD')
+        cy.get('.login-container > .block-customer-login > .block-content > #login-form > .fieldset > .actions-toolbar > div.primary > #send2').click()
+        cy.get(':nth-child(3) > .product-item-info > .product-item-photo > .product-image-container > .product-image-wrapper > .product-image-photo').click()
+    })
+   it('Choose Product Without Login',() => {
+    cy.get('.home-main > img').click()
+    cy.get(':nth-child(1) > .product-item-info > .photo > .product-image-container > .product-image-wrapper > .product-image-photo').click()
+    cy.get(':nth-child(3) > .product-item-info > .photo > .product-image-container > .product-image-wrapper > .product-image-photo').click()
+   })
+   it('Choose Another Product On Men Categories without Login', () => {
+    cy.get('#ui-id-5').click()
+    cy.get(':nth-child(1) > .product-item-info > .product-item-photo > .product-image-container > .product-image-wrapper > .product-image-photo').click()
+    cy.get(':nth-child(3) > .product-item-info > .photo > .product-image-container > .product-image-wrapper > .product-image-photo').click()
+   })
+      
+    })
