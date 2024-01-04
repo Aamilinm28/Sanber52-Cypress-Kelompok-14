24804f9827c4de8a9615c362603ef8373439e58c
mursyidslm <mursyidsalim1@gmail.com>
2024-01-04 15:32:50 +0700

    update code Choose Product With Login

--- cypress/e2e/6-choose-product/choose-product.cy.js
+++ cypress/e2e/6-choose-product/choose-product.cy.js
@@ -5,17 +5,21 @@
 
 describe("Login and Choose Product", () => {
     let signinBtn = '.panel > .header > .authorization-link > a'
+    let Loginbttn = '.login-container > .block-customer-login > .block-content > #login-form > .fieldset > .actions-toolbar > div.primary > #send2'
+    let formeEmail = '#email'
+    let formPassword = '.login-container > .block-customer-login > .block-content > #login-form > .fieldset > .password > .control > #pass'
    
     beforeEach(() => {
         cy.visit('')
     })
 
-    it('Choose Product with Login',() => {
+    it.only('Choose Product with Login',() => {
         cy.get(signinBtn).click()
-        cy.get('#email').type('mursyid@gmail.com')
-        cy.get('.login-container > .block-customer-login > .block-content > #login-form > .fieldset > .password > .control > #pass').type('123qweASD')
-        cy.get('.login-container > .block-customer-login > .block-content > #login-form > .fieldset > .actions-toolbar > div.primary > #send2').click()
+        cy.get(formeEmail).type('mursyid@gmail.com')
+        cy.get(formPassword).type('123qweASD')
+        cy.get(Loginbttn).click(),
         cy.get(':nth-child(3) > .product-item-info > .product-item-photo > .product-image-container > .product-image-wrapper > .product-image-photo').click()
+        cy.get(':nth-child(3) > .product-item-info > .photo > .product-image-container > .product-image-wrapper > .product-image-photo').click()
     })
    it('Choose Product Without Login',() => {
     cy.get('.home-main > img').click()
